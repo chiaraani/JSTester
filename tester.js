063cf6ead66c25ce4c6274ca9191aa18cf216dbf
Chiara Piccinini Núñez <chiaraani@gmail.com>
2021-10-14 01:12:11 +0100

    Replace spaces by tabs

--- tester.js
+++ tester.js
@@ -2,129 +2,129 @@
 /// TESTER ///
 
 class Test {
-  static config = {
-    logSuccessful: true,
-    alertFailure: true,
-    errorGroupCollapsed: true
-  }
+	static config = {
+		logSuccessful: true,
+		alertFailure: true,
+		errorGroupCollapsed: true
+	}
 
-  static executions = []
+	static executions = []
 
-  constructor (description, code) {
-    this.description = description
-    this.execution = this.getExecution(code)
+	constructor (description, code) {
+		this.description = description
+		this.execution = this.getExecution(code)
 
-    this.execution
-      .then( () => this.successMessage() )
-      .catch( error => this.failureMessage(error) )
+		this.execution
+			.then( () => this.successMessage() )
+			.catch( error => this.failureMessage(error) )
 
-    Test.executions.push(this.execution)
-  }
+		Test.executions.push(this.execution)
+	}
 
-  getExecution (code) {
-    if (code.constructor.name === 'AsyncFunction') {
-      return code(testAssert)
-    } else {
-      return (async () => code(testAssert))()
-    }
-  }
+	getExecution (code) {
+		if (code.constructor.name === 'AsyncFunction') {
+			return code(testAssert)
+		} else {
+			return (async () => code(testAssert))()
+		}
+	}
 
-  successMessage () {
-    if (Test.config.logSuccessful) {
-      const message = `%c Success! Test: ${this.description} `
-      const style = 'color: green; background: #dfd;'
-      console.info(message, style)
-    } 
-  }
+	successMessage () {
+		if (Test.config.logSuccessful) {
+			const message = `%c Success! Test: ${this.description} `
+			const style = 'color: green; background: #dfd;'
+			console.info(message, style)
+		} 
+	}
 
-  failureMessage (error) {
-    const message = `%c FAILED! Test: "${this.description}" due to "${error.name}"`
-    const style = 'color: red; background: #fdd; font-weight: bold;'
+	failureMessage (error) {
+		const message = `%c FAILED! Test: "${this.description}" due to "${error.name}"`
+		const style = 'color: red; background: #fdd; font-weight: bold;'
 
-    if (Test.config.errorGroupCollapsed) console.groupCollapsed(message, style)
-    else console.group(message, style)
+		if (Test.config.errorGroupCollapsed) console.groupCollapsed(message, style)
+		else console.group(message, style)
 
-    if (error.name === 'TestAssertionError') console.log(...error.messageArray)
-    console.error(error)
-    console.groupEnd()
-  }
+		if (error.name === 'TestAssertionError') console.log(...error.messageArray)
+		console.error(error)
+		console.groupEnd()
+	}
 
-  static summary (results) {
-    const testCount = results.length
+	static summary (results) {
+		const testCount = results.length
 
-    const passedCount =
-      results
-      .filter( result => result.status === 'fulfilled' )
-      .length
+		const passedCount =
+			results
+			.filter( result => result.status === 'fulfilled' )
+			.length
 
-    const failedCount = testCount - passedCount
-    const allPassed = failedCount === 0
+		const failedCount = testCount - passedCount
+		const allPassed = failedCount === 0
 
-    const background = allPassed ? 'green' : (passedCount > 0 ? 'blue' : 'red')
-    const style = `color: white; background: ${background}; display: block;`
+		const background = allPassed ? 'green' : (passedCount > 0 ? 'blue' : 'red')
+		const style = `color: white; background: ${background}; display: block;`
 
-    const message = `%c ${passedCount} PASSED & ${failedCount} FAILED (tests summary)`
+		const message = `%c ${passedCount} PASSED & ${failedCount} FAILED (tests summary)`
 
-    console.info(message, style)
-    console.groupEnd()
+		console.info(message, style)
+		console.groupEnd()
 
-    if (failedCount && Test.config.alertFailure) {
-      alert('FAILED! Some tests have failed. Check console for more info.')
-    }
-  }
+		if (failedCount && Test.config.alertFailure) {
+			alert('FAILED! Some tests have failed. Check console for more info.')
+		}
+	}
 
-  static case (createTests) {
-    console.group('Tester')
-    createTests()
-    Promise.allSettled(this.executions).then(this.summary)
-  }
+	static case (createTests) {
+		console.group('Tester')
+		createTests()
+		Promise.allSettled(this.executions).then(this.summary)
+	}
 }
 
 function testAssert(arg0, kind = "truthy", ...args) {
-  // Arg abbreviation for argument.
-  args.unshift(arg0)
+	// Arg abbreviation for argument.
+	args.unshift(arg0)
 
-  const asserter =  {
-    // Basic assert kinds //
-    truthy () { Boolean(args[0]) || fail(args[0], 'is NOT truthy') },
-    ['!'] () { !args[0] || fail(args[0], 'is NOT falsy') },
+	const asserter =	{
+		// Basic assert kinds //
+		truthy () { Boolean(args[0]) || fail(args[0], 'is NOT truthy') },
+		['!'] () { !args[0] || fail(args[0], 'is NOT falsy') },
 
-    // Equality assert kinds //
-    ['=='] () { args[0] == args[1]|| fail(args[0], 'does NOT equal', args[1]) },
-    ['!='] () { args[0] != args[1] || fail(args[0], 'DOES equal', args[1]) },
-    ['==='] () { args[0] === args[1] || fail(args[0], 'is NOT equal to', args[1]) },
-    ['!=='] () { args[0] !== args[1] || fail(args[0], 'DOES be equal to', args[1]) },
+		// Equality assert kinds //
+		['=='] () { args[0] == args[1]|| fail(args[0], 'does NOT equal', args[1]) },
+		['!='] () { args[0] != args[1] || fail(args[0], 'DOES equal', args[1]) },
+		['==='] () { args[0] === args[1] || fail(args[0], 'is NOT equal to', args[1]) },
+		['!=='] () { args[0] !== args[1] || fail(args[0], 'DOES be equal to', args[1]) },
 
-    // Array or String assert kinds //
-    includes () { 
-      args[0].includes(args[1]) || fail(args[0], 'does NOT include', args[1])
-    },
-    excludes () { 
-      !args[0].includes(args[1]) || fail(args[0], 'does NOT exclude', args[1])
-    },
+		// Array or String assert kinds //
+		includes () { 
+			args[0].includes(args[1]) || fail(args[0], 'does NOT include', args[1])
+		},
+		excludes () { 
+			!args[0].includes(args[1]) || fail(args[0], 'does NOT exclude', args[1])
+		},
 
-    // String assert kinds //
-    startsWith () { 
-      args[0].startsWith(args[1]) || fail(args[0], 'does NOT start with', args[1])
-    },
-    endsWith () {
-      args[0].endsWith(args[1]) || fail(args[0], 'does NOT end with', args[1])
-    }
-  }
+		// String assert kinds //
+		startsWith () { 
+			args[0].startsWith(args[1]) || fail(args[0], 'does NOT start with', args[1])
+		},
+		endsWith () {
+			args[0].endsWith(args[1]) || fail(args[0], 'does NOT end with', args[1])
+		}
+	}
 
-  asserter[kind]()
-  
-  function fail(...messageArray) {
-    let error =  new TestAssertionError( messageArrayToString(messageArray) )
-    error.messageArray = messageArray
-    throw error
-  }
+	asserter[kind]()
+	
+	function fail(...messageArray) {
+		let error =	new TestAssertionError( messageArrayToString(messageArray) )
+		error.messageArray = messageArray
+		throw error
+	}
 
-  function messageArrayToString(messageArray) {
-    const stringifiedItems = messageArray.map(item => JSON.stringify(item))
-    return stringifiedItems.join(' ')
-  }
+	function messageArrayToString(messageArray) {
+		const stringifiedItems = messageArray.map(item => JSON.stringify(item))
+		return stringifiedItems.join(' ')
+	}
 }
 
-class TestAssertionError extends Error  { name = 'TestAssertionError' }
+class TestAssertionError extends Error	{ name = 'TestAssertionError' }
 
